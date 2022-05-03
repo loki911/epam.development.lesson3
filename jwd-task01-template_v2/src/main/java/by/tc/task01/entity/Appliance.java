4b8f4507d4fa255d17f1a2bc547a502ca3a86af1
Сергей Пасиченко <50148048+loki911@users.noreply.github.com>
2022-05-03 15:31:31 +0300

    Delete jwd-task01-template_v2 directory

--- jwd-task01-template_v2/src/main/java/by/tc/task01/entity/Appliance.java
+++
@@ -1,8 +0,0 @@
-package by.tc.task01.entity;
-
-import java.util.Map;
-
-public abstract class Appliance {
-    public abstract ApplianceTypeEnum getType();
-    public abstract void fillFromMap(Map<String, String> props);
-}
