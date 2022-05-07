470f6f789014654868d09bee845b24491c8d4508
Сергей Пасиченко <50148048+loki911@users.noreply.github.com>
2022-05-07 19:45:48 +0300

    Delete jwd-task01-template_v2 directory

--- jwd-task01-template_v2/src/main/java/by/tc/task01/dao/ApplianceFactory.java
+++
@@ -1,7 +0,0 @@
-package by.tc.task01.dao;
-
-import by.tc.task01.entity.Appliance;
-
-public interface ApplianceFactory {
-    public Appliance createFromString(String str);
-}
--- jwd-task01-template_v2/src/main/java/by/tc/task01/dao/impl/DAOException.java
+++
@@ -1,7 +0,0 @@
-package by.tc.task01.dao.impl;
-
-public class DAOException extends RuntimeException {
-    public DAOException(String errorMessage) {
-        super(errorMessage);
-    }
-}
